dee1b006474a65cf6f1f30ca3b777de94d1b8c36
sowie <dimas.rabelosouza@gmail.com>
2024-01-31 22:51:39 -0300

    Inicio do Projeto de Formulário , aplicação desktop

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Adega_Irmandade
 {
-    partial class Form1
+    partial class frmLogin
     {
         /// <summary>
         /// Variável de designer necessária.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "frmLogin";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
